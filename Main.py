794921558ed3e7a9746e6dfa62f72d019505c00a
lucalice <lucalice76@gmail.com>
2022-04-21 13:07:28 -0500

    update: Main.py

--- Main.py
+++ Main.py
@@ -49,7 +49,7 @@ def turn_on(message):
 def sensor_on(message):
     while True:
         dist = sensor.distance * 100
-        if(dist <= 20):
+        if(dist <= 15):
             bot.reply_to(message,"""Hay un objeto demsiado cerca!!! \nCUIDADO!!!""")
             sleep(1)
         else:
