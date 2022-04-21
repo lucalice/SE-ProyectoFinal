81c8a5f5d2caca6adc01a770421e91b62336178d
lucalice <lucalice76@gmail.com>
2022-04-21 13:02:00 -0500

    update: Main.py

--- Main.py
+++ Main.py
@@ -17,12 +17,12 @@ led = LED(19)
 sensor = DistanceSensor(echo=18, trigger=17)
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-        Hi there, I am LucadevBot.
-        I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-        """)
+Hi there, I am LucadevBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
@@ -30,12 +30,28 @@ def send_welcome(message):
 #def echo_message(message):
 #    bot.reply_to(message, message.text)
 
+
+@bot.message_handler(commands=['apaga'])
+def turn_off(message):
+    led.off()
+    bot.reply_to(message,"""\
+    se apago\
+    """)
+
+@bot.message_handler(commands=['enciende'])
+def turn_on(message):
+    led.on()
+    bot.reply_to(message,"""\
+    se encendio\
+    """)
+
 @bot.message_handler(commands=['analiza'])
 def sensor_on(message):
     while True:
         dist = sensor.distance * 100
-        if(dist <= 10):
-            return "Hay un objeto muy cerca de tu planta. Ten cuidado!!!"
+        if(dist <= 40):
+            bot.reply_to(message,"""Hay un objeto demsiado cerca!!! \nCUIDADO!!!""")
+            sleep(1)
 
 
 bot.infinity_polling()
