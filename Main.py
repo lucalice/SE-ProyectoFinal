e10b6dff136194545ecc5e56c4107b627dd18d3d
lucalice <lucalice76@gmail.com>
2022-04-21 14:19:16 -0500

    update: Main.py

--- Main.py
+++ Main.py
@@ -24,27 +24,6 @@ Hi there, I am LucadevBot.
 I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
 """)
 
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-#@bot.message_handler(func=lambda message: True)
-#def echo_message(message):
-#    bot.reply_to(message, message.text)
-
-
-@bot.message_handler(commands=['apaga'])
-def turn_off(message):
-    led.off()
-    bot.reply_to(message,"""\
-    se apago\
-    """)
-
-@bot.message_handler(commands=['enciende'])
-def turn_on(message):
-    led.on()
-    bot.reply_to(message,"""\
-    se encendio\
-    """)
-
 @bot.message_handler(commands=['analiza'])
 def sensor_on(message):
     while True:
